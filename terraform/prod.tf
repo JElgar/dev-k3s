d73b56593d26b60311956667180ed5295ce8d439
jelgar <jamesnelgar@gmail.com>
2021-03-13 11:01:14 +0000

    Basic nginx deployment on k3s with lets-encrypt

--- terraform/prod.tf
+++ terraform/prod.tf
@@ -1,7 +1,7 @@
-resource "hcloud_server" "prod" {
-  name = "prod"
-  image = "ubuntu-20.04"
-  server_type = "cx11"
-  location = var.location 
-  ssh_keys = ["jelgar@UbuntuPC"]
-}
+#resource "hcloud_server" "prod" {
+#  name = "prod"
+#  image = "ubuntu-20.04"
+#  server_type = "cx11"
+#  location = var.location 
+#  ssh_keys = ["jelgar@UbuntuPC"]
+#}
